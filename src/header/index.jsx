5ff91ec8e8751ec39757e9fba79122c03ce1a899
PsOlga <psyrniko1@gmail.com>
2024-10-04 19:42:24 +0200

    2

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css"
 import logo from "../assets/header-img/logo.svg";
 import basket from "../assets/header-img/Basket.svg"
+
+
 const Header = () => {
 
-  
   return (
     <header className={styles.header}>
       <div className={styles.headerLogo}>
@@ -15,7 +16,7 @@ const Header = () => {
         </Link>
       </div>
       <nav className={styles.headerNav}>
-      <Link to="/pages/home" className={styles.linkHeader}>Main Page</Link>
+        <Link to="/pages/home" className={styles.linkHeader}>Main Page</Link>
         <Link to="/pages/categories" className={styles.linkHeader}>Categories</Link>
         <Link to="/pages/allProductsPage" className={styles.linkHeader}>All products</Link>
         <Link to="/pages/salePage" className={styles.linkHeader}>All sales</Link>
